9923b74c6fbf0da2bfe3898ca64583dcae87901c
Miles Lindheimer <mileslindheimer@gmail.com>
2014-02-08 14:57:59 -0800

    added sphere func

--- Phong_Illumination_Model/example_01.cpp
+++ Phong_Illumination_Model/example_01.cpp
@@ -138,6 +138,53 @@ void circle(float centerX, float centerY, float radius) {
 
   glEnd();
 }
+
+void sphere(float centerX, float centerY, float centerZ, float radius) {
+    // Draw inner circle
+    glBegin(GL_POINTS);
+    
+    // We could eliminate wasted work by only looping over the pixels
+    // inside the sphere's radius.  But the example is more clear this
+    // way.  In general drawing an object by loopig over the whole
+    // screen is wasteful.
+    
+    int i,j;  // Pixel indices
+    
+    int minI = max(0,(int)floor(centerX-radius));
+    int maxI = min(viewport.w-1,(int)ceil(centerX+radius));
+    
+    int minJ = max(0,(int)floor(centerY-radius));
+    int maxJ = min(viewport.h-1,(int)ceil(centerY+radius));
+    
+    
+    
+    for (i=0;i<viewport.w;i++) {
+        for (j=0;j<viewport.h;j++) {
+            
+            // Location of the center of pixel relative to center of sphere
+            float x = (i+0.5-centerX);
+            float y = (j+0.5-centerY);
+            
+            float dist = sqrt(sqr(x) + sqr(y));
+            
+            if (dist<=radius) {
+                
+                // This is the front-facing Z coordinate
+                float z = sqrt(radius*radius-dist*dist);
+                
+                setPixel(i,j, 1.0, 0.0, 0.0);
+                
+                // This is amusing, but it assumes negative color values are treated reasonably.
+                // setPixel(i,j, x/radius, y/radius, z/radius );
+            }
+            
+            
+        }
+    }
+    
+    
+    glEnd();
+}
 //****************************************************
 // function that does the actual drawing of stuff
 //***************************************************

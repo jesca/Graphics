2661c52e7d3a278432593b808656278a0fc16088
jesca <jesca116@gmail.com>
2014-02-09 19:48:04 -0800

    should take all commands

--- Phong_Illumination_Model/example_01.cpp
+++ Phong_Illumination_Model/example_01.cpp
@@ -37,10 +37,17 @@ class Viewport;
 
 class Viewport {
   public:
-    int w, h, f; // width and height
+    int w, h, func; // width and height
     // f is the cmd line option differentiating ambient, specular, diffuse
-    float r, b, g;
+    float ra,ga,ba, rs,gs,bs,rd,gd,rd;
     //rgb are values also passed in from cmd line
+    bool amb,diff;
+   // bool plcount[]={false,false,false,false,false}; //5 point lights total
+    int plcount, dlcount;
+    float pl_array[5][6];
+    float dl_array[5][6];
+    float spec_coeff;
+
 };
 
 
@@ -105,17 +112,14 @@ void circle(float centerX, float centerY, float radius) {
   // screen is wasteful.
 
    int i,j;  // Pixel indices
-    float r,g,b;
     int ka;
     int minI = max(0,(int)floor(centerX-radius));
     int maxI = min(viewport.w-1,(int)ceil(centerX+radius));
     
     int minJ = max(0,(int)floor(centerY-radius));
     int maxJ = min(viewport.h-1,(int)ceil(centerY+radius));
-    float v[] = {0, 0, 1}; // viewer vector
-    r=viewport.r;
-    g=viewport.g;
-    b=viewport.b;
+
+    
     ka=.2;
     
     
@@ -133,7 +137,7 @@ void circle(float centerX, float centerY, float radius) {
                 // This is the front-facing Z coordinate
                 float z = sqrt(radius*radius-dist*dist);
                 
-                if (viewport.f==1)
+                if (viewport.func==1)
                 {
                   r=ka*r;
                   g=ka*g;
@@ -180,18 +184,12 @@ float normalize(int length, float vec[]) {
         vec[i] = vec[i]/vecnorm;
     }
 }
-// Cross Product
-void cross(float a[], float b[], float crossProd[]) {
-    crossProd[0] = a[1]*b[2] - a[2]*b[1];
-    crossProd[1] = a[2]*b[0] - a[0]*b[2];
-    crossProd[2] = a[0]*b[1] - a[1]*b[0];
-}
 
 // Diffuse
 void diffuse(float pixelColor[], float l[], float n[]) {
     normalize(3, l);
     normalize(3, n);
-    float lDotn = fmax(dot(3, l, n), 0);
+    float lDotn = dot(3, l, n);
     for(int i = 0; i < 3; i++) {
         pixelColor[i] *= lDotn;
     }
@@ -201,7 +199,7 @@ void diffuse(float pixelColor[], float l[], float n[]) {
 void specular(float pixelColor[], float r[], float v[], float p) {
     normalize(3, r);
     normalize(3, v);
-    float rDotv = fmax(dot(3, r, v), 0);
+    float rDotv = dot(3, r, v);
     float rDotvP = pow(rDotv, p);
     for(int i = 0; i < 3; i++) {
         pixelColor[i] *= rDotvP;
@@ -209,6 +207,69 @@ void specular(float pixelColor[], float r[], float v[], float p) {
 }
 
 
+void sphere(float centerX, float centerY, float centerZ, float radius) {
+    // Draw inner circle
+    glBegin(GL_POINTS);
+    
+    // We could eliminate wasted work by only looping over the pixels
+    // inside the sphere's radius.  But the example is more clear this
+    // way.  In general drawing an object by loopig over the whole
+    // screen is wasteful.
+    
+    int i,j;  // Pixel indices
+    int r,g,b;
+    int ka,ks,kd; //ambient, diffuse, specular coefficients
+    int minI = max(0,(int)floor(centerX-radius));
+    int maxI = min(viewport.w-1,(int)ceil(centerX+radius));
+    
+    int minJ = max(0,(int)floor(centerY-radius));
+    int maxJ = min(viewport.h-1,(int)ceil(centerY+radius));
+    r=viewport.r;
+    g=viewport.g;
+    b=viewport.b;
+    ka=1;
+    
+    
+    for (i=0;i<viewport.w;i++) {
+        for (j=0;j<viewport.h;j++) {
+            
+            // Location of the center of pixel relative to center of sphere
+            float x = (i+0.5-centerX);
+            float y = (j+0.5-centerY);
+            
+            float dist = sqrt(sqr(x) + sqr(y));
+            
+            if (dist<=radius) {
+                
+                // This is the front-facing Z coordinate
+                float z = sqrt(radius*radius-dist*dist);
+                
+                //ambient
+                if (viewport.f==1)
+                {
+                  r=ka*r;
+                  g=ka*g;
+                  b=ka*b;
+                }
+                //diffuse
+                if (viewport.f==2) {
+
+                }
+
+                setPixel(i,j, r, g, b);
+                
+
+                // This is amusing, but it assumes negative color values are treated reasonably.
+                // setPixel(i,j, x/radius, y/radius, z/radius );
+            }
+            
+            
+        }
+    }
+    
+    
+    glEnd();
+}
 //****************************************************
 // function that does the actual drawing of stuff
 //***************************************************
@@ -261,71 +322,127 @@ int i;
   viewport.b=atof(argv[4]);
 
   
+/*-ka r g b
+This is the ambient color coefficients of the sphere material. The parameters r g b are numbers
+between 0 and 1 inclusive.
+
+• -kd r g b
+This is the diffuse color coefficients of the sphere material. The parameters r g b are numbers
+between 0 and 1 inclusive.
+
+• -ks r g b
+This is the specular color coefficients of the sphere material. The parameters r g b are numbers
+between 0 and 1 inclusive.
+
+• -sp v
+This is the power coefficient on the specular term. It is a number between 0 and max_float.
+
+• -pl x y z r g b
+This adds a point light to the scene. The x y z values are the location of the light. The r g b
+values are it's color. Note that the x y z values are relative to the sphere. That is, the center of
+the sphere is at the origin and the radius of the sphere defines one unit of length. The Y direction
+is UP, the X direction is to the right on the screen, and the Z direction is "in your face." The
+r g b value are between 0 and max_float, NOT between 0 and 1 (that is, the r g b values encode
+the brightness of the light).
+
+• -dl x y z r g b
+This adds a directional light to the scene. The x y z values are the direction that the light
+points in. The r g b values are it's color. See -pl for coordinate system notes
+*/
 
 //ambience
-  if (strcmp(fxn, "-ka\n") == 0) {
-    viewport.f=1;
-       printf ("Characters: %c %c \n", *fxn, 65);
-}
-   if (strcmp(fxn, "-ks\n") == 0) {
-    viewport.f=2;
-       printf ("Characters: %c %c \n", *fxn, 65);
-}
-//diffusion
-if (strcmp(fxn, "-kd\n") == 0) {
-    viewport.f=3;
-       printf ("Characters: %c %c \n", *fxn, 65);
-
-}
 
 
-    //****************************************************
-    // begin Tests
-    //****************************************************
+
+    else if (strcmp(argv[i], "-pl") == 0)
+    {
+      for(int j = 0; j < 6; j++)
+      {
+        pointlight[numpointlights][j] = atof(argv[i + 1 + j]);
+      }
+      numpointlights++;
+    }
+
+    else if (strcmp(argv[i], "-dl") == 0)
+    {
+      for(int j = 0; j < 6; j++)
+      {
+        directionallight[numdirectionallights][j] = atof(argv[i + 1 + j]);
+      }
+      numdirectionallights++;
+
+    }
+  }
+
+
+  for (int a=1; a<=(argc-1); a++) {
+    const char *fxn=argv[a];
+
+    //ambient; -kx r g b
+    if ((strcmp(fxn, "-ka\n") == 0) or strcmp(fxn, "-ka\n") == 0) or strcmp(fxn, "-ka\n") == 0)) {
+    viewport.amb=true;
+    if (strcmp(fxn, "-ka\n") == 0) {
+      viewport.func=1; // change f to indicate function
+      //change rgb values
+    }
+    else if (strcmp(fxn, "-ks\n") == 0) {
+      viewport.func=2;
+    }
+    //diffusion
+    else if (strcmp(fxn, "-kd\n") == 0) {
+      viewport.func=3;
+    }
+    //update rgb values for ambience
+      viewport.ra=atof(argv[2]);
+      viewport.ga=atof(argv[3]);
+      viewport.ba=atof(argv[4]);
+
+    }
+    //specular: -sp v
+    else if ((strcmp(fxn,"-sp\n"))) {
+      viewport.spec=true;
+      viewport.spec_coeff=atof(argv[a+1]);
+    }
+
+    //can have a total of 5 point lights, 5 direction lights, total 10 
+
+    //-pl x y z r g b 
+     if (strcmp(fxn, "-pl\n") == 0) {
+     // for (int pl=0, pl<5;pl++) {
+       // if plcount[pl]==false {
+          for (int addpl=0; addpl<6; addpl++){
+          pl_array[plcount][addpl]=atof(argv[a+1+addpl]);
+        }
+        plcount++;
+    }
+  
+        
+//• -dl x y z r g b
+    if (strcmp(fxn, "-pl\n") == 0) {
+     // for (int dl=0, dl<5;dl++) {
+       // if plcount[dl]==false {
+          for (int adddl=0; adddl<6; adddl++){
+          dl_array[dlcount][addpl]=atof(argv[a+1+adddl]);
+        }
+        dlcount++;
+      }
+  }
+
+
+    // testing functions here
     float test1[] = {3, 4, 2};
     float test2[] = {1, 2, 3};
     float dotp = dot(3, test1, test2);
     printf("should be 17.0: %f\n", dotp);
     
     float normtest = norm(3, test1);
-    printf("should be 5.385165: %f\n", normtest);
-    
-    float crosstest[3];
-    cross(test1, test2, crosstest);
-    printf("should be 8, -7, 2: \n");
-    for(int i = 0; i < 3; i++) {
-        printf("%f \n", crosstest[i]);
-    }
+    printf("should be 5.3851...: %f\n", normtest);
     
     normalize(3, test1);
-    printf("should be 0.557086, 0.742781, 0.371391: \n");
     for(int i = 0; i < 3; i++) {
-        printf("%f \n", test1[i]);
+        printf("%f, \n", test1[i]);
     }
  
-    //****************************************************
-    // end Tests
-    //****************************************************
-
-  /*
-
-    if (fxn.compare("-kd")==1) {
-    viewport.f=2;
-
-           printf ("Decimals: %d %ld\n", viewport.f, 650000L);
-
-  }
-    if (fxn.compare("-ks")==1) {
-        viewport.f=3;
-
-
-   }
-  else{ 
-      std::cerr << "--NOT reading :[." << std::endl;
-
-}*/
-
-
  
 
 

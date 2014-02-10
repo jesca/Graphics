fd4795e10bde0a227cef1a8312c80d561ca76aaf
jesca116@berkeley.edu <jesca116@berkeley.edu>
2014-02-10 02:41:24 -0800

    test

--- Phong_Illumination_Model/example_01.cpp
+++ Phong_Illumination_Model/example_01.cpp
@@ -37,17 +37,24 @@ class Viewport;
 
 class Viewport {
   public:
-    int w, h, f; // width and height
+    int w, h, func; // width and height
     // f is the cmd line option differentiating ambient, specular, diffuse
-    float r, b, g;
+    float ra,ga,ba, rs,gs,bs,rd,gd,rd;
     //rgb are values also passed in from cmd line
+    bool amb,diff;
+   // bool plcount[]={false,false,false,false,false}; //5 point lights total
+    int plcount, dlcount;
+    float pl_array[5][6];
+    float dl_array[5][6];
+    float spec_coeff;
+
 };
 
 
 //****************************************************
 // Global Variables
 //****************************************************
-Viewport	viewport;
+Viewport    viewport;
 
 
 
@@ -95,6 +102,9 @@ void setPixel(int x, int y, GLfloat r, GLfloat g, GLfloat b) {
 // Draw a filled circle.  
 //****************************************************
 
+ 
+
+
 
 void circle(float centerX, float centerY, float radius) {
   // Draw inner circle
@@ -105,39 +115,40 @@ void circle(float centerX, float centerY, float radius) {
   // screen is wasteful.
 
    int i,j;  // Pixel indices
-    float r,g,b;
     int ka;
     int minI = max(0,(int)floor(centerX-radius));
     int maxI = min(viewport.w-1,(int)ceil(centerX+radius));
     
     int minJ = max(0,(int)floor(centerY-radius));
     int maxJ = min(viewport.h-1,(int)ceil(centerY+radius));
-    r=viewport.r;
-    g=viewport.g;
-    b=viewport.b;
     ka=.2;
     
-    
+ 
+
+
     for (i=0;i<viewport.w;i++) {
         for (j=0;j<viewport.h;j++) {
             
             // Location of the center of pixel relative to center of sphere
             float x = (i+0.5-centerX);
             float y = (j+0.5-centerY);
-            
             float dist = sqrt(sqr(x) + sqr(y));
             
             if (dist<=radius) {
-                
                 // This is the front-facing Z coordinate
                 float z = sqrt(radius*radius-dist*dist);
+                /*
+                     For the equation, x + 2y + 2z = 9, the vector A = (1, 2, 2) is a normal vector. |A| = square root of (1+4+4) = 3. 
+     Thus the vector (1/3)A is a unit normal vector for this plane. Also, (-1/3)A is a unit vector. 
+     Unit normal vectors: (1/3, 2/3, 2/3) and (-1/3, -2/3, -2/3)
+    */
+        //getting the unit normal vectors for centerx, centery, 
+
+                float veclen = getLen(x,y,z);
+                float xnorm = x/veclen;
+                float ynorm = y/veclen;
+                float znorm = z/veclen;
                 
-                if (viewport.f==1)
-                {
-                  r=ka*r;
-                  g=ka*g;
-                  b=ka*b;
-                }
 
 
                 setPixel(i,j, r, g, b);
@@ -154,6 +165,13 @@ void circle(float centerX, float centerY, float radius) {
     
     glEnd();
 }
+
+
+//get vector length of 3 variables
+float getLen(float x, float y, float z) {
+    return sqrt(sqr(x)+sqr(y)+sqr(z));
+}
+
 // Dot Product
 float dot(int length, float vec1[], float vec2[]) {
     float sum = 0;
@@ -202,79 +220,26 @@ void specular(float pixelColor[], float r[], float v[], float p) {
 }
 
 
-void sphere(float centerX, float centerY, float centerZ, float radius) {
-    // Draw inner circle
-    glBegin(GL_POINTS);
-    
-    // We could eliminate wasted work by only looping over the pixels
-    // inside the sphere's radius.  But the example is more clear this
-    // way.  In general drawing an object by loopig over the whole
-    // screen is wasteful.
-    
-    int i,j;  // Pixel indices
-    int r,g,b,ka;
-    int minI = max(0,(int)floor(centerX-radius));
-    int maxI = min(viewport.w-1,(int)ceil(centerX+radius));
-    
-    int minJ = max(0,(int)floor(centerY-radius));
-    int maxJ = min(viewport.h-1,(int)ceil(centerY+radius));
-    r=viewport.r;
-    g=viewport.g;
-    b=viewport.b;
-    ka=1;
-    
-    
-    for (i=0;i<viewport.w;i++) {
-        for (j=0;j<viewport.h;j++) {
-            
-            // Location of the center of pixel relative to center of sphere
-            float x = (i+0.5-centerX);
-            float y = (j+0.5-centerY);
-            
-            float dist = sqrt(sqr(x) + sqr(y));
-            
-            if (dist<=radius) {
-                
-                // This is the front-facing Z coordinate
-                float z = sqrt(radius*radius-dist*dist);
-                
-                if (viewport.f==1)
-                {
-                  r=ka*r;
-                  g=ka*g;
-                  b=ka*b;
-                }
+ 
 
-                setPixel(i,j, r, g, b);
-                
 
-                // This is amusing, but it assumes negative color values are treated reasonably.
-                // setPixel(i,j, x/radius, y/radius, z/radius );
-            }
-            
-            
-        }
-    }
-    
-    
-    glEnd();
-}
+
 //****************************************************
 // function that does the actual drawing of stuff
 //***************************************************
 void myDisplay() {
 
-  glClear(GL_COLOR_BUFFER_BIT);				// clear the color buffer
+  glClear(GL_COLOR_BUFFER_BIT);             // clear the color buffer
 
-  glMatrixMode(GL_MODELVIEW);			        // indicate we are specifying camera transformations
-  glLoadIdentity();				        // make sure transformation is "zero'd"
+  glMatrixMode(GL_MODELVIEW);                   // indicate we are specifying camera transformations
+  glLoadIdentity();                     // make sure transformation is "zero'd"
 
 
   // Start drawing
   circle(viewport.w / 2.0 , viewport.h / 2.0 , min(viewport.w, viewport.h) / 3.0);
 
   glFlush();
-  glutSwapBuffers();					// swap buffers (we earlier set double buffer)
+  glutSwapBuffers();                    // swap buffers (we earlier set double buffer)
 }
 
 
@@ -311,22 +276,111 @@ int i;
   viewport.b=atof(argv[4]);
 
   
+/*-ka r g b
+This is the ambient color coefficients of the sphere material. The parameters r g b are numbers
+between 0 and 1 inclusive.
+
+• -kd r g b
+This is the diffuse color coefficients of the sphere material. The parameters r g b are numbers
+between 0 and 1 inclusive.
+
+• -ks r g b
+This is the specular color coefficients of the sphere material. The parameters r g b are numbers
+between 0 and 1 inclusive.
+
+• -sp v
+This is the power coefficient on the specular term. It is a number between 0 and max_float.
+
+• -pl x y z r g b
+This adds a point light to the scene. The x y z values are the location of the light. The r g b
+values are it's color. Note that the x y z values are relative to the sphere. That is, the center of
+the sphere is at the origin and the radius of the sphere defines one unit of length. The Y direction
+is UP, the X direction is to the right on the screen, and the Z direction is "in your face." The
+r g b value are between 0 and max_float, NOT between 0 and 1 (that is, the r g b values encode
+the brightness of the light).
+
+• -dl x y z r g b
+This adds a directional light to the scene. The x y z values are the direction that the light
+points in. The r g b values are it's color. See -pl for coordinate system notes
+*/
 
 //ambience
-  if (strcmp(fxn, "-ka\n") == 0) {
-    viewport.f=1;
-       printf ("Characters: %c %c \n", *fxn, 65);
-}
-   if (strcmp(fxn, "-ks\n") == 0) {
-    viewport.f=2;
-       printf ("Characters: %c %c \n", *fxn, 65);
-}
-//diffusion
-if (strcmp(fxn, "-kd\n") == 0) {
-    viewport.f=3;
-       printf ("Characters: %c %c \n", *fxn, 65);
 
-}
+
+
+    else if (strcmp(argv[i], "-pl") == 0)
+    {
+      for(int j = 0; j < 6; j++)
+      {
+        pointlight[numpointlights][j] = atof(argv[i + 1 + j]);
+      }
+      numpointlights++;
+    }
+
+    else if (strcmp(argv[i], "-dl") == 0)
+    {
+      for(int j = 0; j < 6; j++)
+      {
+        directionallight[numdirectionallights][j] = atof(argv[i + 1 + j]);
+      }
+      numdirectionallights++;
+
+    }
+  }
+
+
+  for (int a=1; a<=(argc-1); a++) {
+    const char *fxn=argv[a];
+
+    //ambient; -kx r g b
+    if ((strcmp(fxn, "-ka\n") == 0) or strcmp(fxn, "-ka\n") == 0) or strcmp(fxn, "-ka\n") == 0)) {
+    viewport.amb=true;
+    if (strcmp(fxn, "-ka\n") == 0) {
+      viewport.func=1; // change f to indicate function
+      //change rgb values
+    }
+    else if (strcmp(fxn, "-ks\n") == 0) {
+      viewport.func=2;
+    }
+    //diffusion
+    else if (strcmp(fxn, "-kd\n") == 0) {
+      viewport.func=3;
+    }
+    //update rgb values for ambience
+      viewport.ra=atof(argv[2]);
+      viewport.ga=atof(argv[3]);
+      viewport.ba=atof(argv[4]);
+
+    }
+    //specular: -sp v
+    else if ((strcmp(fxn,"-sp\n"))) {
+      viewport.spec=true;
+      viewport.spec_coeff=atof(argv[a+1]);
+    }
+
+    //can have a total of 5 point lights, 5 direction lights, total 10 
+
+    //-pl x y z r g b 
+     if (strcmp(fxn, "-pl\n") == 0) {
+     // for (int pl=0, pl<5;pl++) {
+       // if plcount[pl]==false {
+          for (int addpl=0; addpl<6; addpl++){
+          pl_array[plcount][addpl]=atof(argv[a+1+addpl]);
+        }
+        plcount++;
+    }
+  
+        
+//• -dl x y z r g b
+    if (strcmp(fxn, "-pl\n") == 0) {
+     // for (int dl=0, dl<5;dl++) {
+       // if plcount[dl]==false {
+          for (int adddl=0; adddl<6; adddl++){
+          dl_array[dlcount][addpl]=atof(argv[a+1+adddl]);
+        }
+        dlcount++;
+      }
+  }
 
 
     // testing functions here
@@ -343,28 +397,6 @@ if (strcmp(fxn, "-kd\n") == 0) {
         printf("%f, \n", test1[i]);
     }
  
-
-
-
-  /*
-
-    if (fxn.compare("-kd")==1) {
-    viewport.f=2;
-
-           printf ("Decimals: %d %ld\n", viewport.f, 650000L);
-
-  }
-    if (fxn.compare("-ks")==1) {
-        viewport.f=3;
-
-
-   }
-  else{ 
-      std::cerr << "--NOT reading :[." << std::endl;
-
-}*/
-
-
  
 
 
@@ -380,12 +412,12 @@ if (strcmp(fxn, "-kd\n") == 0) {
   glutInitWindowPosition(0,0);
   glutCreateWindow(argv[0]);
 
-  initScene();							// quick function to set up scene
+  initScene();                          // quick function to set up scene
 
-  glutDisplayFunc(myDisplay);				// function to run when its time to draw something
-  glutReshapeFunc(myReshape);				// function to run when the window gets resized
+  glutDisplayFunc(myDisplay);               // function to run when its time to draw something
+  glutReshapeFunc(myReshape);               // function to run when the window gets resized
 
-  glutMainLoop();							// infinite loop that will keep drawing and resizing
+  glutMainLoop();                           // infinite loop that will keep drawing and resizing
   // and whatever else
 
   return 0;

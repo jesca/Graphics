2b91968fa6ca27f09a22f3586bc55e50663ec933
Miles Lindheimer <mileslindheimer@gmail.com>
2014-02-11 22:00:19 -0800

    added vec3 struct, modified functions to work with it

--- Phong_Illumination_Model/example_01.cpp
+++ Phong_Illumination_Model/example_01.cpp
@@ -23,6 +23,7 @@
 
 
 #define PI 3.14159265  // Should be used from mathlib
+#define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
 
 inline float sqr(float x) { return x*x; }
 
@@ -38,12 +39,8 @@ class Viewport {
 public:
     int w, h, func; // width and height
     // f is the cmd line option differentiating ambient, specular, diffuse
-    float ra,ga,ba, rs,gs,bs,rd,gd;
     // bool plcount[]={false,false,false,false,false}; //5 point lights total
-    int plcount, dlcount;
-    float pl_array[5][6];
-    float dl_array[5][6];
-    float spec_coeff;
+    
     
 };
 
@@ -54,6 +51,17 @@ public:
 Viewport    viewport;
 
 
+int plcount=0;
+int dlcount=0;
+float pl_array[5][6];
+float dl_array[5][6];
+float spec_coeff;
+float rgb[3];
+
+// float ambiencergb[3]={0,0,0};
+float ka[]={0,0,0};
+float kd[3]={0,0,0};
+float ks[3]={0,0,0};
 
 
 //****************************************************
@@ -95,62 +103,54 @@ void setPixel(int x, int y, GLfloat r, GLfloat g, GLfloat b) {
     // bug on inst machines.
 }
 
-
-//get vector length of 3 variables
-float getLen(float x, float y, float z) {
-    return sqrt(sqr(x)+sqr(y)+sqr(z));
-}
+//****************************************************
+// Draw a filled circle.
+//****************************************************
+struct Vec3 {
+    float x, y, z;
+};
 
 // Dot Product
-float dot(int length, float vec1[], float vec2[]) {
-    float sum = 0;
-    for (int i = 0; i<length; i++) {
-        sum += vec1[i]*vec2[i];
-    }
-    return sum;
+float dot(Vec3 a, Vec3 b) {
+    return a.x*b.x + a.y*b.y + a.z*b.z;
 }
 
 // Norm (Vector length)
-float norm(int length, float vec[]) {
-    float sum = 0;
-    for (int i = 0; i<length; i++) {
-        sum += vec[i]*vec[i];
-    }
-    return sqrt(sum);
+float norm(Vec3 vec) {
+    return sqrt(dot(vec, vec));
 }
+
 
 // Normalize
-void normalize(int length, float vec[]) {
-    float vecnorm = norm(length, vec);
-    for (int i = 0; i<length; i++) {
-        vec[i] = vec[i]/vecnorm;
-    }
+void normalize(Vec3* vec) {
+    float vecnorm = norm(*vec);
+    vec->x = vec->x/vecnorm;
+    vec->y = vec->y/vecnorm;
+    vec->z = vec->z/vecnorm;
 }
 
+
 // Diffuse
-void diffuse(float pixelColor[], float l[], float n[]) {
-    normalize(3, l);
-    normalize(3, n);
-    float lDotn = dot(3, l, n);
-    for(int i = 0; i < 3; i++) {
-        pixelColor[i] *= lDotn;
-    }
+void diffuse(float diffusePart[], float I[], float l[], float n[]) {
+//    normalize(3, l);
+//    normalize(3, n);
+//    float lDotn = dot(3, l, n);
+//    for(int i = 0; i < 3; i++) {
+//        diffusePart[i] = kd[i] * I[i] * lDotn;
+//    }
 }
 
 // Specular
 void specular(float pixelColor[], float r[], float v[], float p) {
-    normalize(3, r);
-    normalize(3, v);
-    float rDotv = dot(3, r, v);
-    float rDotvP = pow(rDotv, p);
-    for(int i = 0; i < 3; i++) {
-        pixelColor[i] *= rDotvP;
-    }
+//    normalize(3, r);
+//    normalize(3, v);
+//    float rDotv = dot(3, r, v);
+//    float rDotvP = pow(rDotv, p);
+//    for(int i = 0; i < 3; i++) {
+//        pixelColor[i] *= rDotvP;
+//    }
 }
 
-//****************************************************
-// Draw a filled circle.
-//****************************************************
 
 
 
@@ -165,15 +165,29 @@ void circle(float centerX, float centerY, float radius) {
     // screen is wasteful.
     
     int i,j;  // Pixel indices
-    int ka;
     int minI = max(0,(int)floor(centerX-radius));
     int maxI = min(viewport.w-1,(int)ceil(centerX+radius));
     
     int minJ = max(0,(int)floor(centerY-radius));
     int maxJ = min(viewport.h-1,(int)ceil(centerY+radius));
-    ka=.2;
     
+    float final_rgb_diffuse[3];
+    float final_rgb_ambience[3];
+    float final_rgb_specular[3];
+    float final_rgb[3];
     
+    //AMBIANCE
+    // ka * I ... ka=.1kd
+    if (dlcount>1) {
+        for (int k = 0; k < dlcount; k++)
+        {
+            
+            final_rgb_ambience[1] += ka[0] * (dl_array[k][3]);
+            final_rgb_ambience[2] += ka[1] * (dl_array[k][4]);
+            final_rgb_ambience[3] += ka[2] *(dl_array[k][5]);
+        }
+        
+    }
     
     
     for (i=0;i<viewport.w;i++) {
@@ -183,27 +197,49 @@ void circle(float centerX, float centerY, float radius) {
             float x = (i+0.5-centerX);
             float y = (j+0.5-centerY);
             float dist = sqrt(sqr(x) + sqr(y));
+            float dl_I[3];
+            float dl_L[3];
+            float final_rgb_diffuse[3];
+            //float dn_dotproduct;
             
             if (dist<=radius) {
                 // This is the front-facing Z coordinate
                 float z = sqrt(radius*radius-dist*dist);
-                float n[] = {x, y, z};
-                normalize(3, n);
-                normalize(3, viewport.pl_array[0]); // starting with one light source for simplicity, generalize to 5 later...
-                /*
-                 For the equation, x + 2y + 2z = 9, the vector A = (1, 2, 2) is a normal vector. |A| = square root of (1+4+4) = 3.
-                 Thus the vector (1/3)A is a unit normal vector for this plane. Also, (-1/3)A is a unit vector.
-                 Unit normal vectors: (1/3, 2/3, 2/3) and (-1/3, -2/3, -2/3)
-                 */
-                //getting the unit normal vectors for centerx, centery,
-                
-                ///float xnorm = x/veclen;
-                //float ynorm = y/veclen;
-                //float znorm = z/veclen;
+                float xyz[]={x,y,z};
+//                normalize(xyz);
                 
                 
+                for (int m = 0; m<dlcount; m++) {
+                    //I = intensity r g b
+                    dl_I[0]+=dl_array[m][3];
+                    dl_I[1]+=dl_array[m][4];
+                    dl_I[2]+=dl_array[m][5];
+                    //L = direction xyz
+                    dl_L[0]=-dl_array[m][0];//direction
+                    dl_L[1]=-dl_array[m][1];
+                    dl_L[2]=-dl_array[m][2];
+//                    printf(" = %f dl_I = %f\n", xyz, dl_I);
+//                    normalize(dl_I);
+//                    printf("xyz = %f dl_I = %f\n", xyz, dl_I);
+                    
+                    //should already be normalized
+//                    dn_dotproduct=dot(3, dl_L,xyz);
+                    diffuse(final_rgb_diffuse, dl_I, dl_L, xyz);
+//                    for (int i=0; i<3; i++) {
+//                        final_rgb_diffuse[i] += diffusePart[i];
+//                    }
+                    
+                    
+                }
                 
-                setPixel(i,j, viewport.ra, viewport.ga, viewport.ba);
+                
+                
+                for (int x=0; x<3; x++) {
+                    final_rgb[x]+=final_rgb_diffuse[x]+final_rgb_ambience[x]+final_rgb_specular[x];
+                }
+                
+                
+                setPixel(i,j, final_rgb[0], final_rgb[1], final_rgb[2]);
                 
                 
                 // This is amusing, but it assumes negative color values are treated reasonably.
@@ -217,49 +253,6 @@ void circle(float centerX, float centerY, float radius) {
     
     glEnd();
 }
-
-
-<<<<<<< HEAD
-=======
-
-// Dot Product
-float dot(int length, float vec1[], float vec2[]) {
-    float sum = 0;
-    for (int i = 0; i<length; i++) {
-        sum += vec1[i]*vec2[i];
-    }
-    return sum;
-}
-
-// Norm (Vector length)
-float norm(int length, float vec[]) {
-    float sum = 0;
-    for (int i = 0; i<length; i++) {
-        sum += vec[i]*vec[i];
-    }
-    return sqrt(sum);
-}
-
-// Normalize
-float normalize(int length, float vec[]) {
-    float vecnorm = norm(length, vec);
-    for (int i = 0; i<length; i++) {
-        vec[i] = vec[i]/vecnorm;
-    }
-}
-
-// Diffuse
-void diffuse(float pixelColor[], float l[], float n[]) {
-    normalize(3, l);
-    normalize(3, n);
-    float lDotn = dot(3, l, n);
-    for(int i = 0; i < 3; i++) {
-        pixelColor[i] *= lDotn;
-    }
-}
->>>>>>> efe90fa4b8eb282726057cdafbb4b5942696d48c
-
-
 
 
 
@@ -289,12 +282,38 @@ void myDisplay() {
 // the usual stuff, nothing exciting here
 //****************************************************
 
+/*-ka r g b
+ This is the ambient color coefficients of the sphere material. The parameters r g b are numbers
+ between 0 and 1 inclusive.
+ 
+ • -kd r g b
+ This is the diffuse color coefficients of the sphere material. The parameters r g b are numbers
+ between 0 and 1 inclusive.
+ 
+ • -ks r g b
+ This is the specular color coefficients of the sphere material. The parameters r g b are numbers
+ between 0 and 1 inclusive.
+ 
+ • -sp v
+ This is the power coefficient on the specular term. It is a number between 0 and max_float.
+ 
+ • -pl x y z r g b
+ This adds a point light to the scene. The x y z values are the location of the light. The r g b
+ values are it's color. Note that the x y z values are relative to the sphere. That is, the center of
+ the sphere is at the origin and the radius of the sphere defines one unit of length. The Y direction
+ is UP, the X direction is to the right on the screen, and the Z direction is "in your face." The
+ r g b value are between 0 and max_float, NOT between 0 and 1 (that is, the r g b values encode
+ the brightness of the light).
+ 
+ • -dl x y z r g b
+ This adds a directional light to the scene. The x y z values are the direction that the light
+ points in. The r g b values are it's color. See -pl for coordinate system notes
+ */
 
+//ambience
 
 int main(int argc, char *argv[]) {
     
-    int plcount=viewport.plcount;
-    int dlcount=viewport.dlcount;
     
     //This initializes glut
     glutInit(&argc, argv);
@@ -307,145 +326,142 @@ int main(int argc, char *argv[]) {
      printf("\narg%d=%s", i, argv[i]);
      }*/
     
-    
-    
-    
-    
-    /*-ka r g b
-     This is the ambient color coefficients of the sphere material. The parameters r g b are numbers
-     between 0 and 1 inclusive.
-     
-     • -kd r g b
-     This is the diffuse color coefficients of the sphere material. The parameters r g b are numbers
-     between 0 and 1 inclusive.
-     
-     • -ks r g b
-     This is the specular color coefficients of the sphere material. The parameters r g b are numbers
-     between 0 and 1 inclusive.
-     
-     • -sp v
-     This is the power coefficient on the specular term. It is a number between 0 and max_float.
-     
-     • -pl x y z r g b
-     This adds a point light to the scene. The x y z values are the location of the light. The r g b
-     values are it's color. Note that the x y z values are relative to the sphere. That is, the center of
-     the sphere is at the origin and the radius of the sphere defines one unit of length. The Y direction
-     is UP, the X direction is to the right on the screen, and the Z direction is "in your face." The
-     r g b value are between 0 and max_float, NOT between 0 and 1 (that is, the r g b values encode
-     the brightness of the light).
-     
-     • -dl x y z r g b
-     This adds a directional light to the scene. The x y z values are the direction that the light
-     points in. The r g b values are it's color. See -pl for coordinate system notes
-     */
-    
-    //ambience
-    
-    
-    
-    for (int a=1; a<=(argc-1); a++) {
+    for (int a=1; a<argc;) {
         const char *fxn=argv[a];
         
         
         //ambient; -kx r g b
-        if ((strcmp(fxn, "-ka\n") == 0) or (strcmp(fxn, "-ka\n") == 0) or (strcmp(fxn, "-ka\n") == 0)) {
-            printf ("%s \n", "reached first if");
-            if (strcmp(fxn, "-ka\n") == 0) {
-                viewport.func=1; // change f to indicate function
-                //change rgb values
-            }
-            else if (strcmp(fxn, "-ks\n") == 0) {
-                viewport.func=2;
-            }
-            //diffusion
-            else if (strcmp(fxn, "-kd\n") == 0) {
-                viewport.func=3;
-            }
+        if ((strcmp(fxn, "-ka") == 0)) {
+            //  printf ("%s \n", "reached first if");
             //update rgb values for ambience
+            printf ("%s \n", fxn);
             
-            viewport.ra=atof(argv[2]);
-            viewport.ga=atof(argv[3]);
-            viewport.ba=atof(argv[4]);
-            std::cerr << "Usage: " << argv[0] << " <option(s)> SOURCES"
-            << "Options:\n"
-            << "\t-h,--help\t\tShow this help message\n"
-            << "\t-d,--destination DESTINATION\tSpecify the destination path"
-            << std::endl;
+            for (int color=0; color<3; color++){
+                for (int arg=2; arg<=4; arg++){
+                    ka[color]=atof(argv[arg]);
+                    arg++;
+                }
+            }
+            a+=4;
+        }
+        
+        else if ((strcmp(fxn, "-kd") == 0)) {
+            for (int color=0; color<3; color++){
+                for (int arg=2; arg<=4; arg++){
+                    kd[color]=atof(argv[arg]);
+                    arg++;
+                }
+            }
+            a+=4;
+        }
+        
+        else if ((strcmp(fxn, "-ks") == 0)) {
+            printf ("%s \n", fxn);
+            
+            for (int color=0; color<3; color++){
+                for (int arg=2; arg<=4; arg++){
+                    ks[color]=atof(argv[arg]);
+                    arg++;
+                }
+            }
+            a+=4;
+        }
+        
+        
+        
+        //• -dl x y z r g b    -- x y z r g b values stored in a 2 dimensional array, accessed by pl_array[point light number][0-5, with 0 being x and b being 5]
+        else if (strcmp(fxn, "-dl") == 0) {
+            // for (int dl=0, dl<5;dl++) {
+            // if plcount[dl]==false {
+            
+            for (int adddl=0; adddl<6; adddl++){
+                //   printf ("%s \n", argv[a+1+adddl]);
+                //   printf ("%d \n", dlcount);
+                //   printf ("%d \n", adddl);
+                
+                
+                dl_array[dlcount][adddl]=atof(argv[a+1+adddl]);
+            }
+            dlcount++;
+            a+=7;
             
         }
+        
+        
         //specular: -sp v
-        else if ((strcmp(fxn,"-sp\n"))) {
-            viewport.spec_coeff=atof(argv[a+1]);
+        else if ((strcmp(fxn,"-sp"))) {
+            spec_coeff=atof(argv[a+1]);
+            a+=2;
         }
         
         //can have a total of 5 point lights, 5 direction lights, total 10
         
         //-pl x y z r g b
-        if (strcmp(fxn, "-pl\n") == 0) {
+        else if (strcmp(fxn, "-pl") == 0) {
             // for (int pl=0, pl<5;pl++) {
             // if plcount[pl]==false {
             for (int addpl=0; addpl<6; addpl++){
-                viewport.pl_array[plcount][addpl]=atof(argv[a+1+addpl]);
+                pl_array[plcount][addpl]=atof(argv[a+1+addpl]);
             }
             plcount++;
+            a+=7;
         }
         
         
-        //• -dl x y z r g b    -- x y z r g b values stored in a 2 dimensional array, accessed by pl_array[point light number][0-5, with 0 being x and b being 5]
-        if (strcmp(fxn, "-pl\n") == 0) {
-            if (strcmp(fxn, "-dl\n") == 0) {
-                // for (int dl=0, dl<5;dl++) {
-                // if plcount[dl]==false {
-                for (int adddl=0; adddl<6; adddl++){
-                    viewport.dl_array[dlcount][adddl]=atof(argv[a+1+adddl]);
-                }
-                dlcount++;
-            }
+        else {
+            std::cerr << "--arg not recognized" << std::endl;
+            
         }
-        
-        
-        // testing functions here
-        float test1[] = {3, 4, 2};
-        float test2[] = {1, 2, 3};
-        float dotp = dot(3, test1, test2);
-        printf("should be 17.0: %f\n", dotp);
-        
-        float normtest = norm(3, test1);
-        printf("should be 5.3851...: %f\n", normtest);
-        
-        normalize(3, test1);
-        for(int i = 0; i < 3; i++) {
-            printf("%f, \n", test1[i]);
-        }
-        
-        
-        
-        
-        
-        
-        
-        // Initalize theviewport size
-        viewport.w = 400;
-        viewport.h = 400;
-        
-        //The size and position of the window
-        glutInitWindowSize(viewport.w, viewport.h);
-        glutInitWindowPosition(0,0);
-        glutCreateWindow(argv[0]);
-        
-        initScene();                          // quick function to set up scene
-        
-        glutDisplayFunc(myDisplay);               // function to run when its time to draw something
-        glutReshapeFunc(myReshape);               // function to run when the window gets resized
-        
-        glutMainLoop();                           // infinite loop that will keep drawing and resizing
-        // and whatever else
-        
-        return 0;
     }
     
     
+    // testing functions here
+    Vec3 test1;
+    test1.x = 3;
+    test1.y = 4;
+    test1.z = 2;
+    Vec3 test2;
+    test2.x = 1;
+    test2.y = 2;
+    test2.z = 3;
     
+    float dotp = dot(test1, test2);
+    printf("should be 17.0: %f\n", dotp);
+    
+    float normtest = norm(test1);
+    printf("should be 5.3851...: %f\n", normtest);
+    
+    
+    normalize(&test1);
+    printf("should be 0.557086: %f, \n", test1.x);
+    printf("should be 0.742781: %f, \n", test1.y);
+    printf("should be 0.371391: %f, \n", test1.z);
+    
+    
+    
+    
+    
+    
+    // Initalize theviewport size
+    viewport.w = 400;
+    viewport.h = 400;
+    
+    //The size and position of the window
+    glutInitWindowSize(viewport.w, viewport.h);
+    glutInitWindowPosition(0,0);
+    glutCreateWindow(argv[0]);
+    
+    initScene();                          // quick function to set up scene
+    
+    glutDisplayFunc(myDisplay);               // function to run when its time to draw something
+    glutReshapeFunc(myReshape);               // function to run when the window gets resized
+    
+    glutMainLoop();                           // infinite loop that will keep drawing and resizing
+    // and whatever else
+    
+    return 0;
+}
+
     
     
     
